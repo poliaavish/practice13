10a17f0c5bbbb418882524d068859c2ec1e34462
poliaavish <pvishnevska8@gmail.com>
2025-12-02 18:12:56 +0500

    Тест прошел корректно

--- test_calculator.py
+++ test_calculator.py
@@ -8,12 +8,7 @@ def test_subtract():
     assert calculator.subtract(10, 4) == 6
     assert calculator.subtract(0, 5) == -5
 
-def test_addition():
-    # Было: assert 1 + 1 == 2
-    assert 1 + 1 == 2  # Заведомо неверный результат
-
 if __name__ == "__main__":
     test_add()
     test_subtract()
-    test_addition()
     print("Все тесты прошли!")
